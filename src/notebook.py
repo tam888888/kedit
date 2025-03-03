0563486b69b178ddaf76395eb934699d6316dc8d
tam888888 <85535825+tam888888@users.noreply.github.com>
2025-03-03 16:30:09 +0700

    "Add"

---
+++ src/notebook.py
@@ -0,0 +1,23 @@
+import nest_asyncio
+
+nest_asyncio.apply()
+
+import qdrant_client
+
+collection_name="chat_with_docs"
+
+client = qdrant_client.QdrantClient(
+    host="localhost",
+    port=6333
+)
+
+from llama_index.core import SimpleDirectoryReader
+
+input_dir_path = './docs'
+
+loader = SimpleDirectoryReader(
+            input_dir = input_dir_path,
+            required_exts=[".pdf"],
+            recursive=True
+        )
+docs = loader.load_data()
